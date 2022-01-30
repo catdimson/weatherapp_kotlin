9c40d3b63c777ed360941644f1e0ab7f5cf83b28
Dmitry Kotik <k-work31@yandex.ru>
2022-01-30 23:37:48 +0300

    lesson6. Добавляем сервисы

---
+++ app/src/main/java/ru/dkotik/weatherapplication/view/other_receivers/ChangeConnectivityBroadcastReceiver.kt
@@ -0,0 +1,19 @@
+package ru.dkotik.weatherapplication.view.other_receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class ChangeConnectivityBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        StringBuilder().apply {
+            append("СООБЩЕНИЕ ОБ ИЗМЕНЕНИИ ТИПА СОЕДИНЕНИЯ\n")
+            append("Action: ${intent.action}")
+            toString().also {
+                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
